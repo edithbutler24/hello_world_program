bceb99d8ad9954216e9a3f2d6305f12297475b82
edith.butler24 <edith.butler24@d155.org>
2023-11-29 11:13:48 -0600

    added new statement

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("I love computer science :)");
-        
+        System.out.println("beep boop lalala ooga booga");
     }
 }
