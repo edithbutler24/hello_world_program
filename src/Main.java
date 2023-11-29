1c6128e2ed220040bf569e2f72a461fccc1f64b2
edithbutler24 <152317218+edithbutler24@users.noreply.github.com>
2023-11-29 17:28:38 +0000

    replit

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("I love computer science :)");
         System.out.println("beep boop lalala ooga booga");
+      System.out.println("This is written on replit");
     }
 }
